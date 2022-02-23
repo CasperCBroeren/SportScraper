673c68dfe3e817991fa8436c04fb96a1010ec073
Casper Broeren <c.broeren@gmail.com>
2022-02-23 21:57:00 +0100

    Adds fontawesome, edit config and download link in management dashboard

---
+++ SportScraper/ConfigurationExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SportScraper
+{
+    public static class ConfigurationExtension
+    {
+        public static void AddToSection(this IConfiguration configuration, string field, string[] items)
+        {
+
+        }
+    }
+}
---
+++ SportScraper/Pages/EditConfig.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace SportScraper.Pages
+{
+    public class EditConfigModel : PageModel
+    {
+        public string TypeOfConfig { get; private set; }
+        public string[] ConfigItems { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration configuration;
+
+        public EditConfigModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        public void OnGet(string type)
+        {
+            this.TypeOfConfig = type;
+            this.ConfigItems = configuration.GetSection(type.ToLower())
+                .GetChildren()
+                .ToList()
+                .Select(x => x.Value)
+                .ToArray();
+        }
+
+        public void OnPost(string type)
+        {
+
+            this.TypeOfConfig = type;
+            var newCollection = configuration.GetSection(type.ToLower())
+                .GetChildren()
+                .ToList()
+                .Select(x => x.Value)
+                .ToList();
+
+            var newItem = this.Request.Form["field-" + newCollection.Count];
+            newCollection.Add(newItem);
+            configuration.AddToSection(type, newCollection.ToArray());
+            this.ConfigItems = newCollection.ToArray();
+        }
+    }
+}
